67a7b8bfa742cd893d22280d254ddb80332a2ff1
Nick Fisher <nick.fisher@avinium.com>
2024-09-10 13:49:15 +0800

    add CesiumView class

---
+++ lib/src/cesium_view.dart
@@ -0,0 +1,15 @@
+import 'package:vector_math/vector_math.dart';
+
+// Represents the current view (i.e. camera orientation)
+class CesiumView {
+  final Vector3 position;
+  final Vector3 direction;
+  final Vector3 up;
+  final double viewportWidth;
+  final double viewportHeight;
+  final double horizontalFov;
+  final double verticalFov;
+
+  CesiumView(this.position, this.direction, this.up, this.viewportWidth,
+      this.viewportHeight, this.horizontalFov, this.verticalFov);
+}
