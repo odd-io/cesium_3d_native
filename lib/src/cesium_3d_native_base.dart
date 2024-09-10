880badb67a8bad00aaf00caa8d2aba70c6c24435
Nick Fisher <nick.fisher@avinium.com>
2024-09-10 13:48:51 +0800

    add initial Dart API methods

--- lib/src/cesium_3d_native_base.dart
+++ lib/src/cesium_3d_native_base.dart
@@ -1,6 +1,117 @@
-// TODO: Put public facing types in this file.
+import 'dart:ffi';
 
-/// Checks if you are awesome. Spoiler: you are.
-class Awesome {
-  bool get isAwesome => true;
+import 'package:cesium_3d_native/src/cesium_3d_native.g.dart' as g;
+import 'package:cesium_3d_native/src/cesium_view.dart';
+import 'package:ffi/ffi.dart';
+import 'package:vector_math/vector_math.dart';
+
+typedef CesiumTileset = Pointer<g.CesiumTileset>;
+typedef CesiumTile = Pointer<g.CesiumTile>;
+
+class Cesium3D {
+  static bool _initialized = false;
+  static late Pointer<Char> _errorMessage;
+
+  static void _checkInitialized() {
+    if (!_initialized) {
+      g.CesiumTileset_initialize();
+      _errorMessage = calloc<Char>(256);
+      _initialized = true;
+    }
+  }
+
+  ///
+  /// Load a CesiumTileset from a CesiumIonAsset with the specified token.
+  ///
+  static CesiumTileset loadFromCesiumIon(int assetId, String accessToken) {
+    _checkInitialized();
+    final ptr = accessToken.toNativeUtf8(allocator: calloc);
+    final tileset =
+        g.CesiumTileset_createFromIonAsset(assetId, ptr.cast<Char>());
+    calloc.free(ptr);
+    if (tileset == nullptr) {
+      throw Exception("Failed to fetch tileset for Cesium Ion asset $assetId");
+    }
+    return tileset;
+  }
+
+  ///
+  /// Load a CesiumTileset from a url.
+  ///
+  static CesiumTileset loadFromUrl(String url) {
+    _checkInitialized();
+    final ptr = url.toNativeUtf8(allocator: calloc);
+    final tileset = g.CesiumTileset_create(ptr.cast<Char>());
+    calloc.free(ptr);
+    if (tileset == nullptr) {
+      throw Exception("Failed to fetch tileset from url $url");
+    }
+    return tileset;
+  }
+
+  static g.CesiumViewState _toStruct(CesiumView view) {
+    return g.CesiumTileset_createViewState(
+        view.position.x,
+        view.position.y,
+        view.position.z,
+        view.direction.x,
+        view.direction.y,
+        view.direction.z,
+        view.up.x,
+        view.up.y,
+        view.up.z,
+        view.viewportWidth,
+        view.viewportHeight,
+        view.horizontalFov,
+        view.verticalFov);
+  }
+
+  ///
+  /// Load a CesiumTileset from a CesiumIonAsset with the specified token.
+  ///
+  static void updateTilesetView(CesiumTileset tileset, CesiumView view) {
+    int numTiles = g.CesiumTileset_updateView(tileset, _toStruct(view));
+  }
+
+  ///
+  /// Throws an exception if the tileset encountered an error while loading.
+  /// If no exception is thrown, this does not guarantee that the tileset has
+  /// loaded successfully; it may still be pending.
+  ///
+  /// TODO - how to check successful load?
+  ///
+  static void checkLoadError(CesiumTileset tileset) {
+    if (g.CesiumTileset_hasLoadError(tileset) == 1) {
+      g.CesiumTileset_getErrorMessage(tileset, _errorMessage);
+      throw Exception(_errorMessage.cast<Utf8>().toDartString());
+    }
+  }
+
+  ///
+  /// Fetches the root CesiumTile for the tileset.
+  ///
+  static CesiumTile getRootTile(CesiumTileset tileset) {
+    _checkInitialized();
+    final root = g.CesiumTileset_getRootTile(tileset);
+    if (root == nullptr) {
+      throw Exception("No root tile");
+    }
+    return root;
+  }
+
+  ///
+  /// Gets the renderable content for the tileset.
+  ///
+  static CesiumTile getRenderContent(CesiumTileset tileset) {
+    _checkInitialized();
+    return g.CesiumTileset_getRootTile(tileset);
+  }
+
+  ///
+  /// Gets the load state for a given tile.
+  ///
+  static int getLoadState(CesiumTile tile) {
+    _checkInitialized();
+    return g.CesiumTileset_getTileLoadState(tile);
+  }
 }
