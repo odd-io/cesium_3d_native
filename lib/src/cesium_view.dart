6ec90fe5795715dffd2a582e82189c9b89d2e885
Nick Fisher <nick.fisher@avinium.com>
2024-09-12 15:06:30 +0800

    replace vector_math with vector_math_64

--- lib/src/cesium_view.dart
+++ lib/src/cesium_view.dart
@@ -1,4 +1,4 @@
-import 'package:vector_math/vector_math.dart';
+import 'package:vector_math/vector_math_64.dart';
 
 // Represents the current view (i.e. camera orientation)
 class CesiumView {
@@ -7,6 +7,8 @@ class CesiumView {
   final Vector3 up;
   final double viewportWidth;
   final double viewportHeight;
+
+  /// The horizontal field of view of the camera (in radians)
   final double horizontalFov;
 
   CesiumView(this.position, this.direction, this.up, this.viewportWidth,
