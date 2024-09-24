0ba688a8a2fed435c19243980acdedb5d7738510
Nick Fisher <nick.fisher@avinium.com>
2024-09-24 13:33:49 +0800

    build.sh

--- build.sh
+++ build.sh
@@ -13,7 +13,6 @@ echo "Using Cesium Native repository at $CESIUM_REPO"
 cd $CESIUM_REPO 
 mkdir -p build
 
-# Function to build for macOS
 build_macos() {
     for arch in x64 arm64; do
         mkdir -p "build/macos/$arch" 
@@ -41,7 +40,6 @@ build_macos() {
     done
 }
 
-# Function to build for Android
 build_android() {
     for arch in armeabi-v7a arm64-v8a x86 x86_64; do
         mkdir -p "build/android/$arch"
@@ -66,8 +64,76 @@ build_android() {
     done
 }
 
-# Build for macOS
-# build_macos
+build_ios() {
+    for arch in arm64 x86_64; do
+        mkdir -p "build/ios/$arch" 
+        pushd "build/ios/$arch"
+        
+        if [ "$arch" == "arm64" ]; then
+            sdk="iphoneos"
+        else
+            sdk="iphonesimulator"
+        fi
+        
+        # For some reason the default vcpkg dir isn't downloading/keeping the KHR headers
+        # so you need to manually run:
+        # > brew install vcpkg
+        # > vcpkg install ktx:arm64-ios
+        # export VCPKG_INCLUDE_DIR=/path/to/your/manual/vcpkg/installed/arm64-ios/include/
+        
+        cmake -DCMAKE_INCLUDE_PATH=/Users/nickfisher/Documents/vcpkg/installed/arm64-ios/include/ \
+            -DCMAKE_TOOLCHAIN_FILE=/Users/nickfisher/Documents/odd-io/cesium_3d_native/native/ios.toolchain.cmake \
+            -DPLATFORM=OS64 \
+            -DARCHS=arm64 \
+            -DCMAKE_CXX_FLAGS="-I$VCPKG_INCLUDE_DIR" -DCMAKE_SYSTEM_NAME=iOS \
+            -DVCPKG_TRIPLET=arm64-ios -DCESIUM_TESTS_ENABLED=OFF \
+            ..
+        
+        cmake --build . --config Release
+        
+        echo "Building Cesium Native for iOS $arch"
+        static_libs=
+        ezvcpkg_dir=$(grep ezvcpkg cmake_install.cmake | head -n1 | cut -d' ' -f9 | sed 's/^"//; s/"[)]$//; s/packages.*//')
+        echo "ezvcpkg_dir: $ezvcpkg_dir"
+        ezvcpkg_libs=
+        
+        zip_file="cesium-native-v0.39.0-ios-${arch}-release.zip"
+        for file in $(find . -name "*.a"); do
+            if [ ! -f "$zip_file" ]; then
+                zip -j "$zip_file" "$file"
+            else
+                zip -uj "$zip_file" "$file"
+            fi
+        done
+        
+        echo "ezvcpkg: $ezvcpkg_libs"
+        for file in $(find "$ezvcpkg_dir" -path "*$arch*.a" -not -path "*debug*"); do
+            zip -uj "$zip_file" "$file" 
+        done
+        
+        popd
+    done
+    
+    # Create a universal (fat) library
+    mkdir -p "build/ios/universal"
+    pushd "build/ios/universal"
+    
+    for lib in $(find ../arm64 -name "*.a" | xargs -n1 basename); do
+        lipo -create "../arm64/$lib" "../x86_64/$lib" -output "$lib"
+    done
+    
+    zip_file="cesium-native-v0.39.0-ios-universal-release.zip"
+    for file in *.a; do
+        if [ ! -f "$zip_file" ]; then
+            zip -j "$zip_file" "$file"
+        else
+            zip -uj "$zip_file" "$file"
+        fi
+    done
+    
+    popd
+}
 
-# Build for Android
+build_macos
 build_android
+build_ios
