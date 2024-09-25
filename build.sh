1c32d79f2fc93eda9d3dc23be1f51dfee780b217
Nick Fisher <nick.fisher@avinium.com>
2024-09-25 13:13:18 +0800

    add custom curl build for Android

--- build.sh
+++ build.sh
@@ -40,18 +40,67 @@ build_macos() {
     done
 }
 
+# The Android build process is slightly more complicated.
+# We can't use libcurl/libssl from vcpkg, since there's a bug on Android preventing certificates from being opened:
+# https://github.com/openssl/openssl/issues/13565
+# We therefore use BoringSSL, but this means we need to build libcurl from scratch
+
 build_android() {
+
+    if [ -z "$ANDROID_NDK_HOME" ]; then  
+        echo "ANDROID_NDK_HOME must be set";
+        exit -1;
+    fi
+
+    if [ -z "$VCPKG_ROOT" ]; then  
+        echo "VCPKG_ROOT must be set";
+        exit -1;
+    fi
+    
     for arch in armeabi-v7a arm64-v8a x86 x86_64; do
+        
+        # fetch BoringSSL via vcpkg 
+        vcpkg install boringssl:arm64-android
+        VCPKG_ARCH_DIR=$VCPKG_ROOT/installed/arm64-android/
+        OPENSSL_ROOT_DIR=$VCPKG_ARCH_DIR
+
+        # fetch libcurl src via wget & build
+        wget https://curl.se/download/curl-8.10.1.tar.gz && tar -zxf curl-8.10.1.tar.gz 
+        cd curl-8.10.1 && mkdir build
+        cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake \
+            -DANDROID_ABI=$arch \
+            -DANDROID_PLATFORM=android-21 \
+            -DCMAKE_BUILD_TYPE=Release \
+            -DCURL_EXTRA_CONFIGURE_ARGS="--with-libidn2" \
+            -DOPENSSL_ROOT_DIR="$OPENSSL_ROOT_DIR" \
+            -DOPENSSL_SSL_LIBRARY="$OPENSSL_ROOT/lib/libssl.a" \
+            -DOPENSSL_CRYPTO_LIBRARY="$OPENSSL_ROOT/lib/libcrypto.a" \
+            -DOPENSSL_INCLUDE_DIR="$OPENSSL_ROOT/include" \
+            -DBUILD_CURL_EXE=OFF \
+            -DBUILD_SHARED_LIBS=OFF \
+            -DCURL_STATICLIB=ON \
+            -DOPENSSL_USE_STATIC_LIBS=TRUE \
+            ../
+        cmake --build .
+
+        # build Cesium Native
         mkdir -p "build/android/$arch"
         pushd "build/android/$arch"
-        cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake \
-              -DANDROID_ABI=$arch \
+        cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake \
+              -DCMAKE_POSITION_INDEPENDENT_CODE=1 \
+              -DCMAKE_BUILD_TYPE=Release \
+              -DVCPKG_TRIPLET=arm64-android  \
+              -DCESIUM_TESTS_ENABLED=0 \
               -DANDROID_PLATFORM=android-21 \
-              -DCESIUM_TESTS_ENABLED=OFF \
+              -DANDROID_STL=c++_shared \
+              -DANDROID_ABI=arm64-v8a \
+              -DCMAKE_SYSTEM_NAME=Android \
+              -DCMAKE_C_COMPILER=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/darwin-x86_64/bin/clang \
+              -DCMAKE_CXX_COMPILER=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/darwin-x86_64/bin/clang++ \
               ../../../
         cmake --build .
         echo "Building Cesium Native for Android $arch"
-        static_libs=$(find . -name "*.a")
+        static_libs="${VCPKG_ARCH_DIR}/lib/libidn2.a ${VCPKG_ARCH_DIR}/lib/libiconv.a ${VCPKG_ARCH_DIR}/lib/libunistring.a $(find . -name "*.a")"
         zip_file="cesium-native-v0.39.0-android-${arch}-release.zip"
         for file in $static_libs; do
             if [ ! -f "$zip_file" ]; then
@@ -81,13 +130,17 @@ build_ios() {
         # > vcpkg install ktx:arm64-ios
         # export VCPKG_INCLUDE_DIR=/path/to/your/manual/vcpkg/installed/arm64-ios/include/
         
-        cmake -DCMAKE_INCLUDE_PATH=/Users/nickfisher/Documents/vcpkg/installed/arm64-ios/include/ \
-            -DCMAKE_TOOLCHAIN_FILE=/Users/nickfisher/Documents/odd-io/cesium_3d_native/native/ios.toolchain.cmake \
-            -DPLATFORM=OS64 \
-            -DARCHS=arm64 \
-            -DCMAKE_CXX_FLAGS="-I$VCPKG_INCLUDE_DIR" -DCMAKE_SYSTEM_NAME=iOS \
-            -DVCPKG_TRIPLET=arm64-ios -DCESIUM_TESTS_ENABLED=OFF \
-            ..
+        cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake \                                 
+              -DCMAKE_BUILD_TYPE=Release \
+              -DVCPKG_TRIPLET=arm64-android  \
+              -DCESIUM_TESTS_ENABLED=0 \
+              -DANDROID_PLATFORM=android-23 \
+              -DANDROID_STL=c++_shared \
+              -DANDROID_ABI=arm64-v8a \
+              -DCMAKE_ANDROID_ARCH_ABI=arm64-v8a -DCMAKE_SYSTEM_NAME=Android \
+              -DVCPKG_TARGET_ARCHITECTURE=arm64 -DVCPKG_CMAKE_SYSTEM_NAME=Android -DCMAKE_C_COMPILER=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/darwin-x86_64/bin/clang \
+              -DCMAKE_POSITION_INDEPENDENT_CODE=1 -DCMAKE_CXX_COMPILER=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/darwin-x86_64/bin/clang++ \
+              ../../../
         
         cmake --build . --config Release
         
