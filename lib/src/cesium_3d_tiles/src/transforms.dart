ab4694f6ca6a8bf645b615853afcc546aaee16f9
Nick Fisher <nick.fisher@avinium.com>
2024-10-29 17:31:24 +0800

    remove unneeded transform matrices

--- lib/src/cesium_3d_tiles/src/transforms.dart
+++ lib/src/cesium_3d_tiles/src/transforms.dart
@@ -1,27 +1,6 @@
 import 'package:vector_math/vector_math_64.dart';
 import 'dart:math';
 
-final yUpToZUp = Matrix4(
-    1,
-    0,
-    0,
-    0, 
-    0,
-    0,
-    1,
-    0, 
-    0,
-    -1,
-    0,
-    0, 
-    0,
-    0,
-    0,
-    1 
-    );
-
-final zUpToYUp = yUpToZUp.clone()..transpose();
-
 final gltfToEcef = ecefToGltf.clone()..transpose();
 final ecefToGltf =
     Matrix4.rotationZ(-pi / 2) * Matrix4.rotationY(-pi / 2);
